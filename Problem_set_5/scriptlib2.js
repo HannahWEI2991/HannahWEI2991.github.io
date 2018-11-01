75b62cd7c6bdce1a470f9a9186a49e0dc8bc3be4
HannahWEI2991 <43102319+HannahWEI2991@users.noreply.github.com>
2018-11-01 16:30:18 +0800

    Update Problem_set_5/scriptlib2.js
    update js

--- Problem_set_5/scriptlib2.js
+++ Problem_set_5/scriptlib2.js
@@ -1,4 +1,61 @@
-$("button#roll_up").click(function() {
+$(document).ready(function(){
+
+    $("button#hide_h2").click(function() {
+        $("h2").hide(500);
+    });
+
+    $("button#show_h2").click(function() {
+        $("h2").show(300);
+        $("h2").css("color","blue");
+        $("h2").html("You clicked me hard.");
+    });
+
+    $("button#clear_screen").click(function() {
+        var $x = $("container");
+        $x.empty();
+    });
+
+    $("button#get_data").click(function() {
+        var items = [];
+        var i = 0;
+        var airtable_read_endpoint = "https://api.airtable.com/v0/appM38HXlEVhxmnqx/Tasks?api_key=keyTcsTzckqyBTlk8&sortField=_createdTime&sortDirection=desc";
+        var dataSet = [];
+        $.getJSON(airtable_read_endpoint, function(result) {
+               $.each(result.records, function(key,value) {
+                   items = [];
+                       items.push(value.fields.Name);
+                       items.push(value.fields.Stage_Desc);
+                       items.push(value.fields.Completed);
+                       items.push(value.fields.Time_Estimate);
+                       items.push(value.fields.Weight_Factor);
+                       items.push(value.fields.converted);
+                       dataSet.push(items);
+                       console.log(items);
+                }); // end .each
+                console.log(dataSet);
+
+             $('#example').DataTable( {
+                 data: dataSet,
+                 retrieve: true,
+                 columns: [
+                     { title: "Name",
+                       defaultContent:""},
+                     { title: "Stage",
+                         defaultContent:"" },
+                     { title: "Completed",
+                       defaultContent:"" },
+                     { title: "Time Estimated",
+                       defaultContent:""},
+                     { title: "Weight Factor",
+                         defaultContent:""},
+                     { title: "Converted",
+                       defaultContent:""},
+                 ]
+             } );
+        }); // end .getJSON
+     }); // end button
+
+     $("button#roll_up").click(function() {
        var table1_items = [];
        var i = 0;
        var airtable_read_endpoint = "https://api.airtable.com/v0/appM38HXlEVhxmnqx/Tasks?api_key=keyTcsTzckqyBTlk8&sortField=_createdTime&sortDirection=desc";
@@ -62,3 +119,20 @@ $("button#roll_up").click(function() {
                           defaultContent:""},
                     ] // rmf columns
                 } ); // end dataTable
+
+                var chart = c3.generate({
+                     data: {
+                         columns: table2_dataSet,
+                         type : 'bar'
+                     },
+                     donut: {
+                         title: "Tasks for Each Stage:",
+                     }
+                 });
+
+          }); // end .getJSON
+       }); // end button
+
+        // $.getJSON('http://localhost/d756a/data_export.json/Computer+TV', function(obj) {
+
+}); // document ready
