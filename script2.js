f432ba3d2807cb99e392f21511ab7a362104c0ba
HannahWEI2991 <43102319+HannahWEI2991@users.noreply.github.com>
2018-11-03 12:50:59 +0800

    Update script2.js
    update jQuery coding

--- script2.js
+++ script2.js
@@ -1,44 +1,44 @@
- $(document).ready(function(){
-   $("button#button2_1").click(function() {
-        var items = [];
-        var i = 0;
-        var airtable_read_endpoint = "hhttps://api.airtable.com/v0/appQl45FkNG6DrbHy/Restaurant?api_key=keyPGaOFOWVMaoigo&maxRecords=15&view=Grid%20view";
-        var dataSet = [];
-        $.getJSON(airtable_read_endpoint, function(result) {
-               $.each(result.records, function(key,value) {
-                   items = [];
-                       items.push(value.fields.Name);
-                       items.push(value.fields.Location);
-                       items.push(value.fields.Address);
-                       items.push(value.fields.Contact);
-                       items.push(value.fields.Payment_Method);
-                       items.push(value.fields.Average_Cost);
-                       items.push(value.fields.Tags);
-                       dataSet.push(items);
-                       console.log(items);
-                });
-                console.log(dataSet);
-
-             $('#example').DataTable( {
-                 data: dataSet,
-                 retrieve: true,
-                 columns: [
-                     { title: "Name",
-                       defaultContent:""},
-                     { title: "Location",
-                         defaultContent:"" },
-                     { title: "Address",
+$(document).ready(function(){
+  $("button#button2_1").click(function(){
+    var table1_items = [];
+    var i = 0;
+    var airtable_data1 = "https://api.airtable.com/v0/appQl45FkNG6DrbHy/Restaurant?api_key=keyPGaOFOWVMaoigo&maxRecords=15&view=Grid%20view";
+    var table1_dataSet = [];
+     $.getJSON(airtable_data1,function(result){
+       $.each(result.records,function(key,value){
+         table1_items = [];
+         table1_items.push(value.fields.Name);
+         table1_items.push(value.fields.Location);
+         table1_items.push(value.fields.Tags);
+         table1_items.push(value.fields.Contact);
+         table1_items.push(value.fields.Payment_Method);
+         table1_items.push(value.fields.Average_Cost);
+         table1_items.push(value.fields.Address);
+         table1_dataSet.push(table1_items);
+         console.log(table1_items);
+       });
+       console.log(table1_dataSet);
+       $('#table_id_example').DataTable({
+         data:table1_dataSet,
+         retrive:true,
+         columns: [
+                   { title: "Name",
+                     defaultContent:""},
+                   { title: "Location",
                        defaultContent:"" },
-                     { title: "Contact",
+                   { title: "Tags",
+                     defaultContent:"" },
+                   { title: "Contact",
+                     defaultContent:""},
+                   { title: "Payment Method",
                        defaultContent:""},
-                     { title: "Payment Method",
-                         defaultContent:""},
-                     { title: "Average Cost",
+                   { title: "Adverage Cost",
+                     defaultContent:""},
+                   { title: "Address",
                        defaultContent:""},
-                     { title: "Tags",
-                       defaultContent:""},
-                 ]
-             });
-      });
-   });
- });
+                  ]
+
+       });
+     })
+  });
+});
