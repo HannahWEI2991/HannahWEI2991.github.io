6c8349de43a438acb62f843493ec563788b0f0e9
HannahWEI2991 <43102319+HannahWEI2991@users.noreply.github.com>
2018-11-04 11:14:37 +0800

    Update new page for problem_set_5
    create new page using pingendo

--- #problem_set_5_3/script.js
+++ #problem_set_5_3/script.js
@@ -1,27 +1,28 @@
 $(document).ready(function(){
-  $("button#get_data").click(function(){
+
+  $("button#button1").click(function(){
     var table1_items = [];
     var i = 0;
-    var airtable_data1 = "https://api.airtable.com/v0/appQl45FkNG6DrbHy/Restaurant?api_key=keyPGaOFOWVMaoigo&maxRecords=15&view=Grid%20view";
+    var airtable_data = "https://api.airtable.com/v0/appQl45FkNG6DrbHy/Restaurant?api_key=keyPGaOFOWVMaoigo&maxRecords=15&view=Grid%20view"
     var table1_dataSet = [];
-    $.getJSON(airtable_data1,function(result){
-       $.each(result.records,function(key,value){
+
+    $.getJSON(airtable_data,function(result){
+      $.each(result.records,function(key,value){
          table1_items = [];
          table1_items.push(value.fields.Name);
          table1_items.push(value.fields.Location);
          table1_items.push(value.fields.Tags);
          table1_items.push(value.fields.Contact);
          table1_items.push(value.fields.Payment_Method);
-         table1_items.push(value.fields.Average_Cost);
          table1_items.push(value.fields.Address);
          table1_dataSet.push(table1_items);
          console.log(table1_items);
-       });
-       console.log(table1_dataSet);
-       $('#table_id_example').DataTable({
-         data:table1_dataSet,
-         retrive:true,
-         columns: [
+      });
+         console.log(table1_dataSet);
+         $("#table1").DataTable({
+           data:table1_dataSet,
+           retrive:true,
+           columns: [
                    { title: "Name",
                      defaultContent:""},
                    { title: "Location",
@@ -32,13 +33,10 @@ $(document).ready(function(){
                      defaultContent:""},
                    { title: "Payment Method",
                        defaultContent:""},
-                   { title: "Adverage Cost",
-                     defaultContent:""},
                    { title: "Address",
                        defaultContent:""},
                   ]
-
-       });
-     })
+         });
+    });
   });
 });
