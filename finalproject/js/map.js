5cf818d4dfe5386c49b8c6d788878e35e2ef72d9
HannahWEI2991 <43102319+HannahWEI2991@users.noreply.github.com>
2018-11-30 09:38:39 +0800

    update
    add maps and a table

---
+++ finalproject/js/map.js
@@ -0,0 +1,33 @@
+var mapboxTiles1 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a>', maxZoom: 18,})
+var map1 = L.map('map1')
+      .addLayer(mapboxTiles1)
+      .setView([23.039620765729655,113.40595504359848], 15);
+var marker1 = L.marker([23.040620985729655,113.39929990009848])
+       .bindPopup('雅乐轩酒店')
+       .addTo(map1);
+
+
+ var mapboxTiles2 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a>', maxZoom: 18,})
+ var map2 = L.map('map2')
+             .addLayer(mapboxTiles2)
+             .setView([39.9766000000,116.3466100000], 15);
+ var marker2 = L.marker([39.9766000000,116.3466100000])
+              .bindPopup('唯实国际文化交流中心')
+              .addTo(map2);
+
+var mapboxTiles3 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a>', maxZoom: 18,})
+var map3 = L.map('map3')
+              .addLayer(mapboxTiles3)
+              .setView([22.28255283,114.1397851], 18);
+var marker3 = L.marker([22.28255283,114.1397851])
+              .bindPopup('香港大学仪礼堂')
+              .addTo(map3);
+
+
+var mapboxTiles4 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a>', maxZoom: 18,})
+var map4 = L.map('map4')
+              .addLayer(mapboxTiles4)
+              .setView([22.3051363515,114.1792710235], 18);
+var marker4 = L.marker([22.3051363515,114.1792710235])
+              .bindPopup('香港大学仪礼堂')
+              .addTo(map4);
--- finalproject/js/table.js
+++ finalproject/js/table.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
-  var table1_items = [];
-  var i = 0;
-  var airtable_data = "https://api.airtable.com/v0/appxnGEdVS7qDdA5e/requirement?api_key=keyPGaOFOWVMaoigo&maxRecords=17&view=Grid%20view";
-  var table1_dataSet = [];
 
-  $.getJSON(airtable_data,function(result){
-    $.each(result.records,function(key,value){
-       table1_items = [];
+  $("button#button1").click(function(){
+    var table1_items = [];
+    var i = 0;
+    var airtable_data = "https://api.airtable.com/v0/appxnGEdVS7qDdA5e/requirement?api_key=keyPGaOFOWVMaoigo&maxRecords=17&view=Grid%20view"
+    var table1_dataSet = [];
+
+    $.getJSON(airtable_data,function(result){
+      $.each(result.records,function(key,value){
+         table1_items = [];
        table1_items.push(value.fields.专业);
        table1_items.push(value.fields.语言成绩要求);
        table1_items.push(value.fields.是否有笔试面试);
@@ -14,33 +16,31 @@ $(document).ready(function(){
        table1_items.push(value.fields.推荐信要求);
        table1_items.push(value.fields.个人陈述要求);
        table1_items.push(value.fields.是否需要寄送纸质材料);
-       table1_dataSet.push(table1_items);
-       console.log(table1_items);
+         table1_dataSet.push(table1_items);
+         console.log(table1_items);
+      });
+         console.log(table1_dataSet);
+         $("#table1").DataTable({
+           data:table1_dataSet,
+           retrive:true,
+           destroy:true,
+           columns: [
+                   { title: "专业",
+                     defaultContent:""},
+                   { title: "语言成绩要求",
+                       defaultContent:"" },
+                   { title: "是否有面试笔试",
+                     defaultContent:"" },
+                   { title: "是否要求提交作品集",
+                     defaultContent:""},
+                   { title: "推荐信要求",
+                       defaultContent:""},
+                   { title: "个人陈述要求",
+                       defaultContent:""},
+                   { title: "是否要求寄送纸质材料",
+                       defaultContent:""},
+                  ]
+         });
     });
-       console.log(table1_dataSet);
-         //object可以如下初始化表格
-     $("#table1").DataTable({
-         data: table1_dataSet,
-         retrive:true,
-         destroy:true,
-         //使用对象数组，一定要配置columns，告诉 DataTables 每列对应的属性
-         //data 这里是固定不变的，name，position，salary，office 为你数据里对应的属性
-         columns: [
-             { data: "专业",
-               defaultContent:""},
-             { data: "语言成绩要求",
-             defaultContent:""},
-             { data: "是否有笔试面试",
-              defaultContent:""},
-             { data: "是否要求提交作品集",
-              defaultContent:""},
-             { data: "推荐信要求",
-              defaultContent:""},
-             { data: "个人陈述要求",
-              defaultContent:""},
-             { data: "是否需要寄送纸质材料",
-              defaultContent:""},
-         ],
-     });
-});
-});
+  });
+    });
